8a5e52963d745a48b2aad97f7bc07e94db20d09b
F0BiYa <fobiya1982@yandex.ru>
2022-05-17 15:37:45 +0300

    w15 backend users env patch

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -80,7 +80,7 @@ module.exports.createUser = (req, res, next) => {
 /* контроллер Login */
 module.exports.loginUser = (req, res, next) => {
   const { email, password } = req.body;
-  User.findUserByCredentials(email, password)
+  return User.findUserByCredentials(email, password)
     .then((user) => {
       const token = jwt.sign({ _id: user._id }, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret', { expiresIn: '7d' });
       res.send({ token });
